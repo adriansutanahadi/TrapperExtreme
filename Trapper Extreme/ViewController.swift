bcea05aaa57a4d9c309992190b697c3f1c82b188
zelfrontial <denisthamrin@gmail.com>
2015-06-17 22:13:58 +1000

    init board

--- Trapper Extreme/ViewController.swift
+++ Trapper Extreme/ViewController.swift
@@ -4,14 +4,28 @@
 //
 //  Created by Denis Thamrin on 17/06/2015.
 //  Copyright (c) 2015 Denis Thamrin. All rights reserved.
+//  First prototype where model is inside controller
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    var board: [[String?]]! = [[String?]]()
+    
+    
+    func setUpBoard(){
+        for row in 0...3 {
+            for column in 0...3 {
+                self.board[row][column] = nil
+            }
+        }
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpBoard()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
